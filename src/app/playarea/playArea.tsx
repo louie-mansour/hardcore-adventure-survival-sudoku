c7eaab43918ba0710c689d06c78a2b3254e2d2d3
Louie Mansour <louie.m.mansour@gmail.com>
2024-06-25 20:14:08 -0400

    Refactor separating Game and Sudoku

---
+++ src/app/playarea/playArea.tsx
@@ -0,0 +1,154 @@
+import { EndGameError } from "@/errors/endGame";
+import { Game, GameDifficulty } from "@/models/game";
+import { CellType, CellValue, Sudoku } from "@/models/sudoku";
+import { getSudoku } from "@/services/sudokuService";
+import { useEffect, useState } from "react";
+import SudokuArea from "../sudoku/sudokuArea";
+import DifficultySelector from "./difficultySelector";
+import OptionsSelector from "./optionsSelector";
+import Title from "./title";
+
+export enum GameMode {
+  Hardcore = 'Hardcore',
+  Normal = 'Normal',
+  OngoingHints = 'OngoingHints',
+}
+
+export default function PlayArea() {
+  const [game, setGame] = useState<Game>(() => new Game())
+  const [newGame, setNewGame] = useState<Game | null>(null)
+  const [isHardcoreModeEnabled, setIsHardcoreModeEnabled] = useState(true)
+  const [isOngoingHintsModeEnabled, setIsOngoingHintsModeEnabled] = useState(false)
+  // TODO: this renders multiple times. The hack is to set the index explicitly so it's idemptotent
+  const [initialSudoku, setInitialSudoku] = useState<Sudoku>(() => getSudoku({ difficulty: game.difficulty, index: 4 }))
+  const [itemLocations, setItemLocations] = useState<[string, number, number][]>([])
+
+  useEffect(() => {
+    if (initialSudoku && itemLocations.length === 0) {
+      setItemLocations(determineItemLocations(initialSudoku))
+    }
+  }, [initialSudoku])
+
+  useEffect(() => {
+    if (!newGame) {
+      return
+    }
+
+    setGame(g => {
+      try {
+        const nGame = g.newGame(newGame.difficulty)
+        setInitialSudoku(getSudoku({ difficulty: nGame.difficulty }))
+        return nGame
+      } catch (e: unknown) {
+        if (!(e instanceof EndGameError)) {
+          return g
+        }
+        if (!confirm(e.message)) {
+          return g
+        }
+        setInitialSudoku(getSudoku({ difficulty: e.game.difficulty }))
+        return e.game
+      }
+    })
+  }, [newGame])
+
+  return (
+    <div className="w-full h-full	flex flex-row justify-center bg-yellow-50 h-svh">
+      <div className="bg-white w-full max-w-2xl">
+        <div className='h-screen flex flex-col justify-evenly my-0 mx-5 h-svh'>
+          <Title isHardcoreModeEnabled={isHardcoreModeEnabled} isOngoingHintsModeEnabled={isOngoingHintsModeEnabled}/>
+          <DifficultySelector requestNewGame={requestNewGame} difficulty={game.difficulty} />
+          <OptionsSelector
+            enableOngoingHintsMode={enableOngoingHintsMode}
+            enableHardcoreMode={enableHardcoreMode}
+            isHardcoreModeEnabled={isHardcoreModeEnabled}
+            isOngoingHintsModeEnabled={isOngoingHintsModeEnabled}
+          />
+          <div className='flex flex-col justify-center items-center'>
+            <SudokuArea
+              initialSudoku={initialSudoku}
+              gameMode={getMode(isHardcoreModeEnabled, isOngoingHintsModeEnabled)}
+              itemLocations={isHardcoreModeEnabled ? itemLocations : []}
+              solveSudoku={solveSudoku}
+              gameStart={gameStart}
+              gameOver={gameOver}
+              gameComplete={gameComplete}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+
+  function requestNewGame(difficulty: GameDifficulty) {
+    setNewGame(new Game(difficulty))
+  }
+
+  function gameStart() {
+    setGame(g => g.start())
+  }
+
+  function gameOver() {
+    setGame(g => g.fail())
+  }
+
+  function gameComplete() {
+    setGame(g => g.complete())
+  }
+
+  function enableOngoingHintsMode(isEnable: boolean) {
+    setIsOngoingHintsModeEnabled(isEnable)
+  }
+
+  function enableHardcoreMode(isEnable: boolean) {
+    setIsHardcoreModeEnabled(isEnable)
+  }
+
+  function getMode(isHardcoreModeEnabled: boolean, isOngoingHintsModeEnabled: boolean): GameMode {
+    if (isHardcoreModeEnabled) {
+      return GameMode.Hardcore
+    }
+    if (isOngoingHintsModeEnabled) {
+      return GameMode.OngoingHints
+    }
+    return GameMode.Normal
+  }
+
+  function solveSudoku() {
+    setInitialSudoku(s => s.getSolution())
+    setGame(g => g.complete())
+  }
+
+  function determineItemLocations(sudoku: Sudoku): [string, number, number][] {
+    if (!sudoku) return []
+    const availableLocations: [number, number][] = []
+    const cells = sudoku.getCells()
+    for (let r = 0; r < 9; r++) {
+      for (let c = 0; c < 9; c++) {
+        if (cells[r][c].cellType === CellType.Variable) {
+          availableLocations.push([r, c])
+        }
+      }
+    }
+
+    const itemsToDistribute = ['🛡️', '🪄', '🔮', '🔦', '☀️', '🌱', '❄️', '🚒', '🧀', '🖐️']
+    const itemLocations: [string, number, number][] = []
+    for (let i = 0; i < itemsToDistribute.length; i++) {
+      const locationIndex = Math.floor(Math.random() * availableLocations.length)
+      const location = availableLocations[locationIndex]
+      itemLocations.push([itemsToDistribute[i], location[0], location[1]])
+      availableLocations.splice(locationIndex, 1)
+    }
+
+    const negativeEffectsToDistribute = ['🗡️', '🔥', '🐢', '🌋', '🔦', '🪞', '🐀', '😵‍💫']
+
+    for (let i = 0; i < negativeEffectsToDistribute.length; i++) {
+      const locationIndex = Math.floor(Math.random() * availableLocations.length)
+      const location = availableLocations[locationIndex]
+      itemLocations.push([negativeEffectsToDistribute[i], location[0], location[1]])
+      availableLocations.splice(locationIndex, 1)
+    }
+    console.log(itemLocations)
+    return itemLocations
+  }
+}
---
+++ src/app/sudoku/sudokuArea.tsx
@@ -0,0 +1,232 @@
+'use client'
+
+import { CellValue, Sudoku } from "@/models/sudoku";
+import { useEffect, useState } from "react";
+import { GameMode } from "../playarea/playArea";
+import HintPanel from "../sudoku/hintPanel";
+import Sudoku9x9Grid from "../sudoku/sudoku-grid/sudokuGrid";
+import Toolbox from "../sudoku/toolbox";
+
+interface SudokuAreaProps {
+  initialSudoku: Sudoku
+  gameMode: GameMode
+  itemLocations: [string, number, number][],
+  solveSudoku: () => void
+  gameStart: () => void
+  gameOver: () => void
+  gameComplete: () => void
+}
+
+export default function SudokuArea(props: SudokuAreaProps) {
+  const { initialSudoku, gameMode, itemLocations, solveSudoku, gameStart, gameOver, gameComplete } = props
+
+  const [sudoku, setSudoku] = useState<Sudoku>(() => initialSudoku)
+  const [selectedCell, setSelectedCell] = useState<[number, number]>([0, 0])
+  const [notes, setNotes] = useState(() => initNotes())
+  const [mistakes, setMistakes] = useState<[number, number, CellValue][]>([])
+  const [isRevealMistakes, setIsRevealMistakes] = useState(false)
+  const [hint, setHint] = useState<[number, number, CellValue] | null>(null)
+  const [items, setItems] = useState(() => initItems(gameMode))
+  const [enabledItem, setEnabledItem] = useState<string>()
+
+  useEffect(() => {
+    if(sudoku.isSolved()) {
+      gameComplete()
+      return
+    }
+    gameStart()
+  }, [sudoku])
+
+  useEffect(() => {
+    reset()
+  }, [initialSudoku])
+
+  return (
+    <>
+      <div className='flex flex-col justify-center items-center'>
+        <Sudoku9x9Grid
+          selectedCell={selectedCell}
+          selectCell={selectCell}
+          sudoku={sudoku}
+          updateSudoku={updateSudoku}
+          hint={hint}
+          mistakes={isRevealMistakes ? mistakes : []}
+          itemLocations={itemLocations}
+          gameover={gameOver}
+          hasLives={gameMode === GameMode.Hardcore}
+          notes={notes}
+          toggleNoteValue={enabledItem === '✏️' ? toggleNoteValue : undefined}
+        />
+      </div>
+      <Toolbox putValueInCell={putValueInCell} />
+      <HintPanel
+        isFoundMistakes={gameMode === GameMode.OngoingHints ? false : mistakes.length > 0}
+        isFoundHint={!!hint}
+        useItem={useItem}
+        isOngoingHintsModeEnabled={gameMode === GameMode.OngoingHints}
+        isHardcoreModeEnabled={gameMode === GameMode.Hardcore}
+        items={items}
+        enabledItem={enabledItem}
+        chooseEnabledItem={chooseEnabledItem}
+      />
+    </>
+  )
+
+  function reset() {
+    setSudoku(initialSudoku)
+    setNotes(initNotes())
+    setMistakes([])
+    setIsRevealMistakes(false)
+    setHint(null)
+    setItems(initItems(gameMode))
+    setEnabledItem(undefined)
+  }
+
+  function updateSudoku(value: CellValue, row: number, col: number) {
+    switch (gameMode) {
+      case GameMode.Hardcore: return updateSudokuHardcoreBehaviour(value, row, col)
+      case GameMode.OngoingHints: return updateSudokuOngoingHintsBehaviour(value, row, col)
+      case GameMode.Normal: return updateSudokuNormalBehaviour(value, row, col)
+    }
+  }
+
+  function updateSudokuHardcoreBehaviour(value: CellValue, row: number, col: number) {
+    setSudoku(s => {
+      const newSudoku = s.updateCell(value, row, col)
+      const mistakes = newSudoku.findMistakes()
+
+      if (mistakes.length === 0) {
+        const itemLocation = itemLocations.find(el => el[1] === row && el[2] === col)
+        if (value && itemLocation) {
+          setItems(i => [...new Set([...i, itemLocation[0]])])
+        }
+        return newSudoku
+      }
+      setMistakes(mistakes)
+      setIsRevealMistakes(true)
+      return s
+    })
+  }
+
+  function updateSudokuOngoingHintsBehaviour(value: CellValue, row: number, col: number) {
+    setSudoku(s => {
+      const newSudoku = s.updateCell(value, row, col)
+      const mistakes = newSudoku.findMistakes()
+  
+      if (mistakes.length === 0) {
+        return newSudoku
+      }
+      setMistakes(mistakes)
+      setIsRevealMistakes(true)
+      setHint(newSudoku.getHint({ allowMistakes: true }))
+      return s
+    })
+  }
+
+  function updateSudokuNormalBehaviour(value: CellValue, row: number, col: number) {
+    setSudoku(s => {
+      const newSudoku = s.updateCell(value, row, col)
+
+      setHint(h => {
+        if (h && h[0] === row && h[1] === col) {
+          return null
+        }
+        return h
+      })
+      setMistakes(m => {
+        if (m.map(m => `${m[0]}${m[1]}`).includes(`${row}${col}`)) {
+          const newMistakes = m.filter(m => m[0] !== row || m[1] !== col)
+  
+          if (newMistakes.length === 0) {
+            setIsRevealMistakes(false)
+          }
+          return newMistakes
+        }
+        return m
+      })
+
+      return newSudoku
+    })
+  }
+
+  function revealHint() {
+    const revealedHint = sudoku.revealHint()
+    const newSudoku = sudoku.updateCell(revealedHint[2], revealedHint[0], revealedHint[1])
+    setSudoku(newSudoku)
+
+    if (gameMode === GameMode.OngoingHints) {
+      setMistakes(newSudoku.findMistakes())
+      setHint(newSudoku.getHint({ allowMistakes: true }))
+      return
+    }
+
+    setHint(null)
+  }
+
+  function selectCell(cell: [number, number]) {
+    setSelectedCell(cell)
+  }
+
+  function putValueInCell(value: CellValue) {
+    if (enabledItem === '✏️') {
+      toggleNoteValue(selectedCell[0], selectedCell[1], value)
+    } else {
+      updateSudoku(value, selectedCell[0], selectedCell[1])
+    }
+  }
+
+  function initNotes(): Set<CellValue>[][] {
+    return Array.from(
+      { length: 9 }, _r => Array.from(
+        { length: 9 }, _c => new Set<CellValue>([])
+      )
+    )
+  }
+
+  function toggleNoteValue(row: number, col: number, noteValue: CellValue) {
+    if (notes[row][col].has(noteValue)) {
+      notes[row][col].delete(noteValue)
+    } else {
+      notes[row][col].add(noteValue)
+    }
+    setNotes([...notes])
+  }
+
+  function chooseEnabledItem(item: string) {
+    setEnabledItem(i => {
+      if (item === i) {
+        return undefined
+      }
+      return item
+    })
+  }
+
+  function useItem(value: string) {
+    switch (value) {
+      case '🗑️': return updateSudoku(null, selectedCell[0], selectedCell[1])
+      case '🔍': {
+        if (mistakes.length > 0) {
+          return setIsRevealMistakes(true)
+        }
+        if (hint) {
+          return revealHint()
+        }
+      }
+      case '🍼':
+        return revealHint()
+      case '🏳️': return solveSudoku()
+      default:
+        return alert('Not implemented yet')
+    }
+  }
+
+  function initItems(gameMode: GameMode): string[] {
+    if (gameMode === GameMode.Hardcore) {
+      return ['✏️', '🧯']
+    }
+    if (gameMode === GameMode.OngoingHints) {
+      return ['✏️', '🗑️', '🍼', '🏳️']
+    }
+    return ['✏️', '🔍', '🏳️']
+  }
+}
